b7703c7ee9dbc8ce209909216379ab598d00b88d
evsik <evsevsikov@gmail.com>
2020-12-12 14:16:26 +0300

    added animation(2)

--- scripts/app.js
+++ scripts/app.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {
+$(document).ready(function () {
     $('.second-slider').addClass("hidden").viewportChecker({
         classToAdd: 'visible animate__animated animate__fadeInUp',
         offset: 300
@@ -10,5 +10,25 @@ $(document).ready(function() {
     $('.pluses-article').addClass("hidden").viewportChecker({
         classToAdd: 'visible animate__animated animate__fadeInLeft',
         offset: 300
+    });
+    $('.about-us-wrapper').addClass("hidden").viewportChecker({
+        classToAdd: 'visible animate__animated animate__fadeIn',
+        offset: 300
+    });
+    $('.archive-block-img').addClass("hidden").viewportChecker({
+        classToAdd: 'visible animate__animated animate__fadeIn',
+        offset: 200
+    });
+    $('.invisible-block-active').addClass("hidden").viewportChecker({
+        classToAdd: 'visible animate__animated animate__slideInUp',
+        offset: 200
+    });
+    $('.slider-gallery-block').addClass("hidden").viewportChecker({
+        classToAdd: 'visible animate__animated animate__zoomIn',
+        offset: 200
+    });
+    $('.our-team-block').addClass("hidden").viewportChecker({
+        classToAdd: 'visible animate__animated animate__slideInUp',
+        offset: 200
     });
 });
