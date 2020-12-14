79385b88fd398dac6ac9d4f838bcd19622a521be
evsik <evsevsikov@gmail.com>
2020-12-14 11:36:27 +0300

    added animation for slider

--- scripts/app.js
+++ scripts/app.js
@@ -1,34 +1,34 @@
-// $(document).ready(function () {
-//     $('.second-slider').addClass("hidden").viewportChecker({
-//         classToAdd: 'visible animate__animated animate__fadeInUp',
-//         offset: 300
-//     });
-//     $('.pluses-article-right').addClass("hidden pluses-article-right").viewportChecker({
-//         classToAdd: 'visible animate__animated animate__fadeInRight',
-//         offset: 300
-//     });
-//     $('.pluses-article').addClass("hidden").viewportChecker({
-//         classToAdd: 'visible animate__animated animate__fadeInLeft',
-//         offset: 300
-//     });
-//     $('.about-us-wrapper').addClass("hidden").viewportChecker({
-//         classToAdd: 'visible animate__animated animate__fadeIn',
-//         offset: 300
-//     });
-//     $('.archive-block-img').addClass("hidden").viewportChecker({
-//         classToAdd: 'visible animate__animated animate__fadeIn',
-//         offset: 200
-//     });
-//     $('.invisible-block-active').addClass("hidden").viewportChecker({
-//         classToAdd: 'visible animate__animated animate__slideInUp',
-//         offset: 200
-//     });
-//     $('.slider-gallery-block').addClass("hidden").viewportChecker({
-//         classToAdd: 'visible animate__animated animate__zoomIn',
-//         offset: 200
-//     });
-//     $('.our-team-block').addClass("hidden").viewportChecker({
-//         classToAdd: 'visible animate__animated animate__slideInUp',
-//         offset: 200
-//     });
-// });
+$(document).ready(function () {
+    $('.second-slider').addClass("hidden").viewportChecker({
+        classToAdd: 'visible animate__animated animate__fadeInUp',
+        offset: 300
+    });
+    $('.pluses-article-right').addClass("hidden pluses-article-right").viewportChecker({
+        classToAdd: 'visible animate__animated animate__fadeInRight',
+        offset: 300
+    });
+    $('.pluses-article').addClass("hidden").viewportChecker({
+        classToAdd: 'visible animate__animated animate__fadeInLeft',
+        offset: 300
+    });
+    $('.about-us-wrapper').addClass("hidden").viewportChecker({
+        classToAdd: 'visible animate__animated animate__fadeIn',
+        offset: 300
+    });
+    $('.archive-block-img').addClass("hidden").viewportChecker({
+        classToAdd: 'visible animate__animated animate__fadeIn',
+        offset: 200
+    });
+    $('.invisible-block-active').addClass("hidden").viewportChecker({
+        classToAdd: 'visible animate__animated animate__slideInUp',
+        offset: 200
+    });
+    $('.slider-gallery-block').addClass("hidden").viewportChecker({
+        classToAdd: 'visible animate__animated animate__zoomIn',
+        offset: 200
+    });
+    $('.our-team-block').addClass("hidden").viewportChecker({
+        classToAdd: 'visible animate__animated animate__slideInUp',
+        offset: 200
+    });
+});
